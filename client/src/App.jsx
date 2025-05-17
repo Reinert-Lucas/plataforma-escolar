35e45cb82d3c27c5d4e795ac2d3efe5def41623e
Lucas Iván Reinert <lucasreinert2006@gmail.com>
2025-05-17 14:45:10 -0300

    .

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      {/* <Routes>
+        <Route path="/" element={<MainView />} />
+      </Routes> */}
+    </>
+  )
+}
+
+export default App

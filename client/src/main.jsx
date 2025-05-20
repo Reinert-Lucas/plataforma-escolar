00bc40ad7cf24db36fe4d27bf961b1789b3c4d87
Lucas Iván Reinert <lucasreinert2006@gmail.com>
2025-05-20 14:29:18 -0300

    delete chakra

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react'
 import { BrowserRouter } from 'react-router-dom';
-import { createRoot } from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './App.jsx'
@@ -9,9 +7,7 @@ import App from './App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <ChakraProvider>
         <App />
-      </ChakraProvider>
     </BrowserRouter>
   </StrictMode>,
 )
